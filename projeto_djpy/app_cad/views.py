a9672043a16c57d440456dac9ecb3f3c03bec1ef
Andrews Almeida Silva <andrews.s.h.e.a.a.s.1@gmail.com>
2023-11-03 16:38:35 -0300

    atualização 03/11

--- projeto_djpy/app_cad/views.py
+++ projeto_djpy/app_cad/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-
+from .models import Usuario
 # Create your views here.
 
+
 def home(request):
-    return render(request,'usuarios/home.html')
+    return render(request, 'usuarios/home.html')
+
+# Salvar os dados da tela para o banco de dados.
+
 
 def usuarios(request):
-pass
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    # Exibir todos os usuarios já cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    # retonar os dados para a página de listagem de usuários
+    return render(request, 'usuarios/usuarios.html', usuarios)
